df3d4b29cf376b847f2a314aaa9cbd61899ae1aa
abeldiva <abelbenita51@gmail.com>
2023-05-22 09:28:22 -0700

    _atoi.c

---
+++ _atoi.c
@@ -0,0 +1,72 @@
+#include "shell.h"
+
+/**
+ * interactive - if shell is interactive true
+ * @info: address struct
+ *
+ * Return: 1 interactive mode, 0 otherwise
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - character delimeter
+ * @c: char check
+ * @delim: string delimeter
+ * Return: 0 if false, 1 if true
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+/**
+ * _isalpha - alphabetic check
+ * @c: input character
+ * Return:  0 otherwise, 1 if alphabetic,
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+/**
+ * _atoi - changes string to an integer
+ * @s: to convert
+ * Return: zero in string, convert number otherwise
+ */
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
